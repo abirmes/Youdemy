fc653079373c74886cb92f5c1384bac4279df8d2
abirmes <abirmeskini02@gmail.com>
2025-01-12 17:24:29 +0100

    les classes

---
+++ app/Models/Cour.php
@@ -0,0 +1,80 @@
+<?php
+
+class Cour
+{
+    private int $id;
+    private string $titre;
+    private string $description;
+    private string $enseignant;
+    private string $contenu;
+    private Categorie $categorie;
+    private array $tags = [];
+
+
+    public function __construct() {}
+
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+    public function setTitre(string $titre): void
+    {
+        $this->titre = $titre;
+    }
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+    public function setEnseignant(string $enseignant): void
+    {
+        $this->enseignant = $enseignant;
+    }
+    public function setContenu(string $contenu): void
+    {
+        $this->contenu = $contenu;
+    }
+    public function setTag(array $tags): void
+    {
+        $this->tags = $tags;
+    }
+    public function setCategorie(Categorie $categorie): void
+    {
+        $this->categorie = $categorie;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+    public function getTitre(): string
+    {
+        return $this->titre;
+    }
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+    public function getEnseignant(): string
+    {
+        return $this->enseignant;
+    }
+    public function getContenu(): string
+    {
+        return $this->contenu;
+    }
+    public function getTags(): array
+    {
+        return $this->tags;
+    }
+    public function getCategorie(): Categorie
+    {
+        return $this->categorie;
+    }
+
+
+    public function __toString()
+    {
+        
+    }
+}
